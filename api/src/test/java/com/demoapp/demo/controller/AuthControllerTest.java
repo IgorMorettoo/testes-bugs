a924ba40507c7c2bd4da973d948f644d0fdc1140
IgorMorettoo <ifmoretto@minha.fag.edu.br>
2026-06-24 13:54:28 -0300

    Ajustado teste de reset de senha para CI

--- api/src/test/java/com/demoapp/demo/controller/AuthControllerTest.java
+++ api/src/test/java/com/demoapp/demo/controller/AuthControllerTest.java
@@ -1,15 +1,14 @@
 package com.demoapp.demo.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -76,6 +75,6 @@ public class AuthControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(resetBody))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("E-mail enviado com sucesso"));
+                .andExpect(jsonPath("$.message").value("Senha redefinida com sucesso (fake)"));
     }
 }
